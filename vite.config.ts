43c1e6c85e292614688e41c7573c3ab4679dbf3c
enoch-prince <63630599+enoch-prince@users.noreply.github.com>
2026-05-09 23:25:26 +0000

    BugFix: assets exceeding the limit

--- vite.config.ts
+++ vite.config.ts
@@ -41,8 +41,11 @@ export default defineConfig({
           },
         ],
       },
+      injectManifest: {
+        maximumFileSizeToCacheInBytes: 30 * 1024 * 1024,
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2,wasm}"],
+      },
       workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2}"],
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
