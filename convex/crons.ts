86820b5b46fc44a0028d5aa48eb334a1e1f1320f
enoch-prince <63630599+enoch-prince@users.noreply.github.com>
2026-05-15 22:43:14 +0000

    feature: cron job to send reminder twice daily

--- convex/crons.ts
+++ convex/crons.ts
@@ -4,10 +4,18 @@ import { internal } from "./_generated/api";
 const crons = cronJobs();
 
 // Schedule the reminder to run every day at 18:00 (6:00 PM) UTC
-crons.daily(
+// crons.daily(
+//   "daily-expense-reminder",
+//   { hourUTC: 18, minuteUTC: 0 },
+//   internal.push.sendReminders
+// );
+
+// Schedule the reminder to run every day at 12:00 & 18:00 (6:00 PM) UTC
+crons.cron(
   "daily-expense-reminder",
-  { hourUTC: 18, minuteUTC: 0 },
-  internal.push.sendReminders
+  "0 12,18 * * *",
+  internal.push.sendReminders,
+  {}
 );
 
 export default crons;
